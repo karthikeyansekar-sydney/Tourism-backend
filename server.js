6f9620765c340bc32aea9e76f6e10e66f2e0382f
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-09-25 00:20:44 +1000

    frontend and backend connection

--- server.js
+++ server.js
@@ -10,7 +10,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes); // => /api/places...
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
+app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
@@ -22,12 +33,12 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 mongoose
-.connect('mongodb+srv://KartMong:s7olvpGebRRd249f@cluster0.3j5jv.mongodb.net/places?retryWrites=true&w=majority',{
+.connect('mongodb+srv://KartMong:s7olvpGebRRd249f@cluster0.3j5jv.mongodb.net/mern?retryWrites=true&w=majority',{
 useNewUrlParser: true,
 useUnifiedTopology: true
 })
